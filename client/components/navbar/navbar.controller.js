7717a748d2f2a391a21d85a18e694a718d50670e
Luke Gary <lukegary@gmail.com>
2014-09-01 14:45:36 -0400

    added anchor scroll logic to top for menu

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ngLukeGaryApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+  .controller('NavbarCtrl', function ($scope, $location, Auth, $anchorScroll) {
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
@@ -19,5 +19,13 @@ angular.module('ngLukeGaryApp')
 
     $scope.isActive = function(route) {
       return route === $location.path();
+    };
+
+    $scope.scrollTo = function(id) {
+        var old = $location.hash();
+        $location.hash(id);
+        $anchorScroll();
+        //reset to old to keep any additional routing logic from kicking in
+        $location.hash(old);
     };
   });
