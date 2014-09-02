6bee7444d650f2c95046ca49686149ec41a7a14f
Luke Gary <lukegary@gmail.com>
2014-09-02 13:41:00 -0400

    allow cors

--- client/components/socket/socket.service.js
+++ client/components/socket/socket.service.js
@@ -8,7 +8,8 @@ angular.module('ngLukeGaryApp')
     var ioSocket = io('', {
       // Send auth token on connection, you will need to DI the Auth service above
       // 'query': 'token=' + Auth.getToken()
-      path: '/socket.io-client'
+      path: '/socket.io-client',
+      origins: '*'
     });
 
     var socket = socketFactory({
